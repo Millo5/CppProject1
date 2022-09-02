129d4bcaf05ec8148b0b2feb77bea675aa3db6b0
Millo <23leeuw@gmail.com>
2022-09-02 15:25:53 +0200

    Update Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -10,6 +10,8 @@ int main()
 	int width = 800;
 	int height = 800;
 
+	int x = 0;
+
 	struct bar
 	{
 
@@ -27,7 +29,9 @@ int main()
 		// Drawing
 		window.clear(sf::Color::Black);
 		sf::RectangleShape bar(sf::Vector2f(50.f, 100.f));
-		bar.setPosition(width / 2, height / 2);
+		bar.setPosition(x, height / 2);
+
+		x += 1;
 
 		// Displaying
 		window.draw(bar);
